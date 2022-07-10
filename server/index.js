775aac7e9253b9a207734e7fb4d2e54b914cf56c
Shreya <kundushreya312@gmail.com>
2022-07-10 19:04:25 +0530

    heroku

--- server/index.js
+++ server/index.js
@@ -45,8 +45,10 @@ app.listen(PORT, () => {
 })
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
- app.use(express.static('client/build'));
+	app.use(express.static('client/build'));
+	 app.use(express.static("admin/build"));
  app.get('*', (req, res) => {
- res.sendFile(path.join(__dirname + '/client/build/index.html'));
+	 res.sendFile(path.join(__dirname + '/client/build/index.html'));
+	 res.sendFile(path.join(__dirname + "/admin/build/index.html"));
  });
 }
